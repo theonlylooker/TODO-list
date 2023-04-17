a36ad8a04a70c6201b848d1a5544f732cf2684f8
basurco eduardo <angelsoulsword@gmail.com>
2023-04-17 12:02:57 -0500

    feat: handling with localStorage with arrays

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const clear = () => {
   form.elements.taskHours.value = "";
   form.elements.taskDescription.value = "";
 };
+//ready
 const submit = (e) => {
   e.preventDefault();
   let formData = {
@@ -13,91 +14,114 @@ const submit = (e) => {
     description: form.elements.taskDescription.value,
   };
   clear();
-
-  if (localStorage.length > 0) {
-    const newEvent = new Event("inserted");
-    localStorage.setItem(localStorage.length + 1, JSON.stringify(formData));
+  let Tasks = localStorage.getItem("Tasks");
+  const newEvent = new Event("inserted");
+  if (Tasks === null) {
+    let tasks = [formData];
+    localStorage.setItem("Tasks", [JSON.stringify(tasks)]);
     document.dispatchEvent(newEvent);
     return;
   }
-  const newEvent = new Event("inserted");
-  localStorage.setItem(1, JSON.stringify(formData));
+  let tasks = JSON.parse(localStorage.getItem("Tasks"));
+  tasks.push(formData);
+  localStorage.setItem("Tasks", JSON.stringify(tasks));
   document.dispatchEvent(newEvent);
   return;
 };
+
 const done = (id) => {
-  let key = id;
-  localStorage.removeItem(key);
   const deleteElement = new Event("deleted");
+  let key = id;
+  let tasks = JSON.parse(localStorage.getItem("Tasks"));
+  tasks.splice(id, 1);
+  //let result = [...tasks.slice(0, id), ...tasks.slice(id + 1)];
+  localStorage.setItem("Tasks", JSON.stringify(tasks));
   document.dispatchEvent(deleteElement);
 };
 
-const newtask = (params) => {
-  let key = localStorage.length;
-  let newTask = JSON.parse(localStorage.getItem(key));
-  let div = `<div class="task">
-                    <div>
-                        <p class="taskHeader">${newTask.name}</p>
-                        <hr/>
-                    </div>
-                    <p class="taskID">${key}</p>
-                    <p class="taskHour">The number of hours to complete the task is: ${newTask.hours}</p>
-                    <p class="taskDescription">The description of the task is: ${newTask.description}</p>
-                    <hr/>
-                    <div>
-                        <button class="taskDone" name="taskDone" data-id="${key}">Hecho</button>
-                    </div>
-                    
-            </div>`;
-  list.insertAdjacentHTML("beforeend", div);
+//ready
+const newtask = () => {
+  let tasks = JSON.parse(localStorage.getItem("Tasks"));
+  let newTask = tasks.slice(-1);
+  // let div = document.createElement("div");
+  // div.classList.add("task");
+  // div.innerHTML = `
+  //             <div>
+  //               <p class="taskHeader">${newTask[0].name}</p>
+  //               <hr/>
+  //             </div>
+  //             <p class="taskHour">The number of hours to complete the task is: ${
+  //               newTask[0].hours
+  //             }</p>
+  //             <p class="taskDescription">The description of the task is: ${
+  //               newTask[0].description
+  //             }</p>
+  //             <hr/>
+  //             <div>
+  //               <button class="button taskDone" onclick="done(${
+  //                 tasks.length - 1
+  //               })"  name="taskDone" data-id="${
+  //   tasks.length - 1
+  // }">Hecho</button>
+  //             </div>`;
+  list.appendChild(
+    makeTask(
+      newTask[0].name,
+      newTask[0].hours,
+      newTask[0].description,
+      tasks.length - 1
+    )
+  );
 };
 
+//ready
 const tasks = () => {
-  if (localStorage.length < 0) {
+  let tasks = JSON.parse(localStorage.getItem("Tasks"));
+  list.innerHTML = ``;
+  if (tasks === null) {
     return;
   }
-  list.innerHTML = ``;
-  for (let i = 0; i < localStorage.length; i++) {
-    let task = JSON.parse(localStorage.getItem(i + 1));
-    let div = `<div class="task">
-                    <div>
-                        <h3 class="taskHeader">${task.name}</h3>
-                        <hr/>
-                    </div>
-                    <p class="taskHour">The number of hours to complete the task is: ${
-                      task.hours
-                    }</p>
-                    <p class="taskDescription">The description of the task is: ${
-                      task.description
-                    }</p>
-                    <hr/>
-                    <div>
-                        <button class="button" name="taskDone" data-id="${
-                          i + 1
-                        }"">Hecho</button>
-                    </div>
-                </div>`;
-    list.insertAdjacentHTML("beforeend", div);
-    console.log(task);
-  }
+  const fragment = new DocumentFragment();
+  tasks.forEach((task, index) => {
+    fragment.appendChild(
+      makeTask(task.name, task.hours, task.description, index)
+    );
+  });
+  list.appendChild(fragment);
   const doneButtons = document.querySelectorAll(".taskDone");
   doneButtons.forEach((button) =>
     button.addEventListener("click", (e) => {
-      console.log("click", button);
+      console.log(e.target.dataset.id, "asd");
       done(e.target.dataset.id);
     })
   );
-  const clearButton = document.querySelector(".clearButton");
-  console.log(clearButton, "button");
-
-  clearButton.addEventListener("click", (e) => {
-    localStorage.clear();
-    const deleteElement = new Event("deleted");
-    document.dispatchEvent(deleteElement);
-  });
 };
+
+const clearButton = document.querySelector(".clearButton");
+clearButton.addEventListener("click", (e) => {
+  const deleteElement = new Event("deleted");
+  localStorage.clear();
+  document.dispatchEvent(deleteElement);
+});
 
 form.addEventListener("submit", submit);
 document.addEventListener("inserted", newtask);
 window.addEventListener("load", tasks);
 document.addEventListener("deleted", tasks);
+
+const makeTask = (name, hours, description, key) => {
+  let div = document.createElement("div");
+  div.classList.add("task");
+  div.innerHTML = `
+                  <div>
+                    <h3 class="taskHeader">${name}</h3>
+                    <hr/>
+                  </div>
+                  <p class="taskHour">The number of hours to complete the task is: ${hours}</p>
+                 <p class="taskDescription">The description of the task is: ${description}</p>
+                  <hr/>
+                  <div>
+                    <button class="button taskDone" name="taskDone" data-id="${key}"">Hecho</button>
+                  </div>`;
+  return div;
+};
